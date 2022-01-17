2eecc5c5cbf45f0710ed8dadce80ba33035cd663
Yannik Wailzer <yannik.wailzer@htl.rennweg.at>
2022-01-17 14:11:56 +0100

    Remove try/catch

--- stats.php
+++ stats.php
@@ -15,25 +15,20 @@
 <h3><i class="mr-10 icon-envelope"></i>Referrers</h3>
 <?php
 $file = './data/refs.json';
-try {
-    $data = json_decode(file_get_contents($file));
-    echo '<table>';
-    echo '<th>Referrer tag</th><th>Number of visits</th>';
-    foreach ($data as $key => $value) {
-        $key = htmlspecialchars($key);
-        $value = htmlspecialchars($value);
-        echo "
-        <tr>
-            <td>$key</td>
-            <td>$value</td>
-        </tr>";
-    }
-    echo '</table>';
-} catch(e) {
-    echo '<code>Could not read data</code>';
-    error_log(e);
-}
+$data = json_decode(file_get_contents($file));
 
+echo '<table>';
+echo '<th>Referrer tag</th><th>Number of visits</th>';
+foreach ($data as $key => $value) {
+    $key = htmlspecialchars($key);
+    $value = htmlspecialchars($value);
+    echo "
+    <tr>
+        <td>$key</td>
+        <td>$value</td>
+    </tr>";
+}
+echo '</table>';
 ?>
 </body>
 </html>
