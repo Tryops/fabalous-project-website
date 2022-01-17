46cd1c7bb6e2388ba5adb34d6369bc0625297daa
Yannik Wailzer <yannik.wailzer@htl.rennweg.at>
2022-01-17 14:20:57 +0100

    Fix file error handling

--- stats.php
+++ stats.php
@@ -14,21 +14,28 @@
 <h1><i class="mr-10 icon-chart"></i>Statistics</h1>
 <h3><i class="mr-10 icon-envelope"></i>Referrers</h3>
 <?php
-$file = './data/refs.json';
-$data = json_decode(file_get_contents($file));
+try {
+    set_error_handler(function($errno, $errstr) { throw new Exception($errstr, $errno); }, E_WARNING);
 
-echo '<table>';
-echo '<th>Referrer tag</th><th>Number of visits</th>';
-foreach ($data as $key => $value) {
-    $key = htmlspecialchars($key);
-    $value = htmlspecialchars($value);
-    echo "
-    <tr>
-        <td>$key</td>
-        <td>$value</td>
-    </tr>";
+    $file = './data/refs.json';
+    $data = json_decode(file_get_contents($file));
+
+    echo '<table>';
+    echo '<th>Referrer tag</th><th>Number of visits</th>';
+    foreach ($data as $key => $value) {
+        $key = htmlspecialchars($key);
+        $value = htmlspecialchars($value);
+        echo "
+        <tr>
+            <td>$key</td>
+            <td>$value</td>
+        </tr>";
+    }
+    echo '</table>';
+} catch(\Exception $e) {
+    echo '<code>Could not read data</code>';
 }
-echo '</table>';
+
 ?>
 </body>
 </html>
